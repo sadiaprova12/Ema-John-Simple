3a6c8c344dbca474cdaf491a3b20b06b8c39cf55
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-20 22:45:41 +0600

    import images logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,18 @@
+/* eslint-disable no-unused-vars */
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import './Header.css'
+import logo from '../../images/Logo.svg'
 const Header = () => {
     return (
         <div className='header'>
-            
+            <img src={logo} alt="" />
+            <div>
+            <a href="/order">Order</a>
+            <a href="/order review">Order Reviwes</a>
+            <a href="/inventory">Manage Inventory</a>
+            <a href="/login">Login</a>
+            </div>
         </div>
     );
 };
