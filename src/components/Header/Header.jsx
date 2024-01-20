61a6b0f9dbaae35c3a2fa4fccbdb3f33be3f3ce0
sadiaprova12 <sadiaprova51717@gmail.com>
2024-01-20 22:30:38 +0600

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-
+import './Header.css'
 const Header = () => {
     return (
-        <div>
-            <h2>This is a header</h2>
+        <div className='header'>
+            
         </div>
     );
 };
