0188092aa4aabc4e9e6b6b775fafd6d5ba3bbd30
sadiaprova12 <sadiaprova51717@gmail.com>
2024-02-11 19:10:52 +0600

    added product

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './Product.css';
+
+
+const Product = () => {
+    return (
+        <div>
+            <h3>This is Product</h3>
+        </div>
+    );
+};
+
+export default Product;
