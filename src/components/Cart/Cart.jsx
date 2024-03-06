136829b769ab36262aeb5d67a1f0c041ee21a319
sadiaprova12 <sadiaprova51717@gmail.com>
2024-03-06 22:12:31 +0600

    total product price

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,15 +2,23 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import './Cart.css'
-const Cart = (props) => {
+const Cart = ({cart}) => {
     // eslint-disable-next-line react/prop-types
-    // const cart = props.cart;
-    const {cart} = props;
+    // const cart = props.cart; // option 1
+    // const {cart} = props; // option 2
+
+    console.log(cart);
+
+    let total = 0;
+    for (const product of cart){
+        total = total + product.price;
+    }
+
     return (
         <div className='cart'>
             <h4>Oder Summary</h4>
                 <p>Selected Items: {cart.length}</p>
-                <p>Total Price: </p>
+                <p>Total Price: {total}</p>
                 <p>Total Shipping: </p>
                 <p>Tax: </p>
                 <p>Grand Total: </p>
