a483cf6d2cf359160e8123b005da0b55efb46341
sadiaprova12 <sadiaprova51717@gmail.com>
2024-03-06 21:43:01 +0600

    added cart length

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,11 +4,16 @@ import React from 'react';
 import './Cart.css'
 const Cart = (props) => {
     // eslint-disable-next-line react/prop-types
-    const cart = props.cart;
+    // const cart = props.cart;
+    const {cart} = props;
     return (
-        <div>
+        <div className='cart'>
             <h4>Oder Summary</h4>
                 <p>Selected Items: {cart.length}</p>
+                <p>Total Price: </p>
+                <p>Total Shipping: </p>
+                <p>Tax: </p>
+                <p>Grand Total: </p>
         </div>
     );
 };
