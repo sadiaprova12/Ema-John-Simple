72636f1b2800a33dd455ef41f24afbf4ef435160
sadiaprova12 <sadiaprova51717@gmail.com>
2024-03-07 11:11:33 +0600

    added cart component and calculation

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -10,18 +10,25 @@ const Cart = ({cart}) => {
     console.log(cart);
 
     let total = 0;
+    let totalShipping = 0;
+    // let tax = 0;
     for (const product of cart){
         total = total + product.price;
+        totalShipping = totalShipping + product.price;
+        // tax = tax + product.price;
     }
+    const tax = total*7/100;
+
+    const grandTotal = total + totalShipping + tax;
 
     return (
         <div className='cart'>
             <h4>Oder Summary</h4>
                 <p>Selected Items: {cart.length}</p>
                 <p>Total Price: {total}</p>
-                <p>Total Shipping: </p>
-                <p>Tax: </p>
-                <p>Grand Total: </p>
+                <p>Total Shipping: ${totalShipping}</p>
+                <p>Tax: ${tax.toFixed(2)}</p>
+                <p>Grand Total: ${grandTotal.toFixed(2)}</p>
         </div>
     );
 };
