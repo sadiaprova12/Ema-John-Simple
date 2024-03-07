3e915f982c0e87f7aa4bb7ece354107e43926519
sadiaprova12 <sadiaprova51717@gmail.com>
2024-03-07 17:34:51 +0600

    retrieve product from stored cart and set it to cart state or display local storage cart to the UI

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -18,25 +18,45 @@ function Shop() {
             .then(data => setProducts(data));
     }, []);
 
-    useEffect( () => {
-        console.log('products', products);
-        const storedCart = getShoppingCart
-        // console.log(storedCart);
+    // useEffect( () => {
+    //     console.log('products', products);
+    //     const storedCart = getShoppingCart
+    //     // console.log(storedCart);
 
-        // Step 1: get id
-        for(const id in storedCart){
-            // console.log(id);
+    //     // Step 1: get id
+    //     for(const id in storedCart){
+    //         // console.log(id);
 
-            // step 2: get the product by using id
-            const addedProduct = products.find(product => product.id === id)
-            console.log(addedProduct);
+    //         // step 2: get the product by using id
+    //         const addedProduct = products.find(product => product.id === id)
+    //         console.log(addedProduct);
 
-            // step 3: get quantity of the product
+    //         // step 3: get quantity of the product
+    //         const quantity = storedCart[id];
+    //         addedProduct.quantity = quantity
+    //         console.log(addedProduct);
+    //     }
+
+    // }, [products])
+
+    useEffect( () =>{
+        const storedCart = getShoppingCart();
+        const savedcart = [];
+        // Step-1: get id of the addedProduct
+        for (const id in storedCart){
+            //Step-2: get product from products state by using id
+        const addedProduct = products.find(product => product.id === id)
+        if(addedProduct){
+            //Step-3: Add quantity
             const quantity = storedCart[id];
-            addedProduct.quantity = quantity
-            console.log(addedProduct);
+            addedProduct.quantity = quantity;
+            //Step-4: add the addedProduct to the savedcart
+            savedcart.push(addedProduct);
         }
-
+        console.log('added Product', addedProduct)
+        }
+        //Step-5: Set the cart
+        setCart(savedcart);
     }, [products])
 
     const handleAddToCart = (product) => {
