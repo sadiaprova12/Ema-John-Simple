156dbceae74f719853379ca5a8b54469e3a98359
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-03 21:50:04 +0600

    nested route

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,44 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Layout/Home.jsx';
+import Shop from './components/Shop/shop.jsx';
+import Orders from './components/Orders/Orders.jsx';
+import Inventory from './components/Inventory/Inventory.jsx';
+import Login from './components/Login/Login.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/',
+        element: <Shop></Shop>
+      },
+      {
+        path: 'orders',
+        element: <Orders></Orders>
+      },
+      {
+        path: 'inventory',
+        element: <Inventory></Inventory>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      }
+    ]
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
     <App />
   </React.StrictMode>,
 )
