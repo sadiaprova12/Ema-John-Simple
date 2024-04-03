ca42d94a068ecdd5cffe4b9575426b7baaa13487
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-03 21:58:33 +0600

    added use loader data products

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,12 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import Cart from '../Cart/Cart';
+import { useLoaderData } from 'react-router-dom';
 
 const Orders = () => {
+    const products = useLoaderData();
+    console.log(products);
     return (
         <div className='shop-container'>
             <div className='products-container'>
-            <h2>Orders Page</h2>
+            <h2>Orders Page: {products.length}</h2>
             </div>
             <div className='cart-container'>
                 <Cart cart={[]}></Cart>
