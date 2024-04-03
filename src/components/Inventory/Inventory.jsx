d3d1be862138f4a1c95964c67562cdf3b4cddf19
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-03 21:48:36 +0600

    inventory page

---
+++ src/components/Inventory/Inventory.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Inventory = () => {
+    return (
+        <div>
+            <h2>Inventory Page</h2>
+        </div>
+    );
+};
+
+export default Inventory;
