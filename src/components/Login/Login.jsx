27fd49c50f02f001b7a225814ecf5692cbfcb186
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-03 21:49:15 +0600

    login page

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h3>Login Commoning Soon..........</h3>
+        </div>
+    );
+};
+
+export default Login;
