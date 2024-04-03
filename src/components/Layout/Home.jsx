a964919d47822532ad04c14c000847747130fee1
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-03 21:48:58 +0600

    header and outlate added

---
+++ src/components/Layout/Home.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import Header from '../Header/Header';
+import { Outlet } from 'react-router-dom';
+
+const Home = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Home;
