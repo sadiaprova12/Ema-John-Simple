0a2369b792b8db8cec550226f2e7dfa547a1dd38
sadiaprova12 <sadiaprova51717@gmail.com>
2024-04-05 00:30:33 +0600

    checkout order

---
+++ src/components/CheckOut/CheckOut.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const CheckOut = () => {
+    return (
+        <div>
+            <h2>Checkout your order!!!!</h2>
+        </div>
+    );
+};
+
+export default CheckOut;
